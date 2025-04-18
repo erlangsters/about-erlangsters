ec971dad640564ae59f2f9c1521dfffd43022f41
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2025-04-19 02:30:18 +0800

    Update 'Packages Collection' page (draft)

--- src/pages/packages-collection.vue
+++ src/pages/packages-collection.vue
@@ -1,3 +1,59 @@
 <template>
-  <main>Packages Collection</main>
+<main class="container">
+  <div class="text-white text-center">
+    <h3>Packages Collection</h3>
+    <p class="w-50 mx-auto">
+Just a curated list of libraries and applications that are well-tested,
+well-documented, and (should) have a good reputation in the BEAM community.
+    </p>
+    <p class="w-50 fst-italic mx-auto">
+All compatible with rebar3 and erlang.mk
+    </p>
+  </div>
+  <div class="text-white text-left fs-5 mb-3">Foo</div>
+  <ul class="list-group mb-3">
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+      A list item
+      <span class="badge text-bg-primary rounded-pill">14</span>
+    </li>
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+      A second list item
+      <span class="badge text-bg-primary rounded-pill">2</span>
+    </li>
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+      A third list item
+      <span class="badge text-bg-primary rounded-pill">1</span>
+    </li>
+  </ul>
+  <div class="text-white text-left fs-5 mb-3">Foo</div>
+  <ul class="list-group mb-3">
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+      A list item
+      <span class="badge text-bg-primary rounded-pill">14</span>
+    </li>
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+      A second list item
+      <span class="badge text-bg-primary rounded-pill">2</span>
+    </li>
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+      A third list item
+      <span class="badge text-bg-primary rounded-pill">1</span>
+    </li>
+  </ul>
+  <div class="text-white text-left fs-5 mb-3">Foo</div>
+  <ul class="list-group mb-3">
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+      A list item
+      <span class="badge text-bg-primary rounded-pill">14</span>
+    </li>
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+      A second list item
+      <span class="badge text-bg-primary rounded-pill">2</span>
+    </li>
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+      A third list item
+      <span class="badge text-bg-primary rounded-pill">1</span>
+    </li>
+  </ul>
+</main>
 </template>
